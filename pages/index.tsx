d7a760d8aa8cffaf9799ab9eebeaf142feaccbab
wam <ami6577@colorverse.co>
2023-05-30 23:14:39 +0900

    #2.5 Dynamic Routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home({
     <div className="container">
       {results?.map((movie: any) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -19,6 +19,9 @@ export default function Home({
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
